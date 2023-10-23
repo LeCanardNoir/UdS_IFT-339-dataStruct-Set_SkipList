465e5165483fc8249c468d75a0472a0a938d9f51
LeCanardNoir <dev.lecanardnoir@gmail.com>
2023-10-23 15:37:37 -0400

    INIT: Project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿// Set_SkipList.cpp : définit le point d'entrée de l'application.
+//
+
+#include "set.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
