daabd783675df8ac69095fa62997f0d284795abc
2160630 <2160630@cegepmontpetit.ca>
2023-10-24 17:05:10 -0400

    BUG: lower_bound

--- main.cpp
+++ main.cpp
@@ -9,5 +9,19 @@ using namespace std;
 int main()
 {
 	cout << "Hello CMake." << endl;
+
+	set<int> s;
+	s.insert(3);
+	s.insert(4);
+	s.insert(7);
+	afficher(cout, s);
+
+	set<int> s2;
+	afficher(cout, s2);
+
+	s2 = set(s);
+	afficher(cout, s2);
+
+
 	return 0;
 }
